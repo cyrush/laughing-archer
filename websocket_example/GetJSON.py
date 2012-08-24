09fa377d33e034a75d259e423800175d26f260f5
Cyrus Harrison <cyglor@gmail.com>
2012-08-23 17:34:01 -0700

    added mings example

---
+++ websocket_example/GetJSON.py
@@ -0,0 +1,51 @@
+import os
+from visit import *
+from convert_obj_three import convert_ascii, convert_binary
+
+def GetJSON(filebase="visit", TYPE="ascii"):
+    # Save the window as OBJ file format.
+    swa = GetSaveWindowAttributes()
+    swa.fileName = filebase
+    swa.family = 0
+    swa.format = swa.OBJ
+    SetSaveWindowAttributes(swa)
+    name = SaveWindow()
+
+    # If there was a .visit file, get the contents of it.
+    filenames = []
+    s = os.stat(name)
+    if s.st_size == 0:
+        visitfile = name[:-4] + ".visit"
+        try:
+            s = os.stat(visitfile)
+            filenames = [ x[:-1] for x in open(visitfile, "rt").readlines()[1:]]
+        except OSError:
+            pass
+    else:
+        filenames = [name]
+
+    # Convert each of the files to JSON.
+    jsons = []
+    for f in filenames:
+        outfile = f[:-4] + ".js"
+        if TYPE == "ascii":
+            morphfiles = ""
+            colorfiles = ""
+            convert_ascii(f, morphfiles, colorfiles, outfile)
+        else:
+            convert_binary(f, outfile)
+        # Read the json back in
+        json = open(outfile, "rb").read()
+        jsons = jsons + [json]
+    return jsons
+
+# #####################################################
+# Main
+# #####################################################
+if __name__ == "__main__":
+    OpenDatabase("~/data/noise.silo")
+    AddPlot("Pseudocolor", "hardyglobal")
+    DrawPlots()
+
+    jsons = GetJSON(filebase="testing", TYPE="ascii")
+    print jsons
